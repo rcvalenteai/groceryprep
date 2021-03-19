9ec2608a3f461053116d522be1adc96cd20c1424
rcvalenteai <44619673+rcvalenteai@users.noreply.github.com>
2021-03-19 15:01:50 -0400

    amplify

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:groceryprep/main.dart';
+import 'package:groceryprep/old-main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
