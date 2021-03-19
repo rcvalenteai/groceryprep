0c1b4e1ed03e74f5a5dbc893ce931f1715726aa5
rcvalenteai <44619673+rcvalenteai@users.noreply.github.com>
2021-03-19 15:58:44 -0400

    added amplifyconfig.dart

--- lib/amplifyconfiguration.dart
+++ lib/amplifyconfiguration.dart
@@ -1,4 +1,52 @@
 const amplifyconfig = ''' {
     "UserAgent": "aws-amplify-cli/2.0",
-    "Version": "1.0"
+    "Version": "1.0",
+    "analytics": {
+        "plugins": {
+            "awsPinpointAnalyticsPlugin": {
+                "pinpointAnalytics": {
+                    "appId": "e2786728adf2428ea309a183f7ce84ec",
+                    "region": "us-east-1"
+                },
+                "pinpointTargeting": {
+                    "region": "us-east-1"
+                }
+            }
+        }
+    },
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:807af47a-c017-447c-90b5-e73bce08718f",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                },
+                "PinpointAnalytics": {
+                    "Default": {
+                        "AppId": "e2786728adf2428ea309a183f7ce84ec",
+                        "Region": "us-east-1"
+                    }
+                },
+                "PinpointTargeting": {
+                    "Default": {
+                        "Region": "us-east-1"
+                    }
+                }
+            }
+        }
+    }
 }''';
