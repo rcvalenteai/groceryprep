47886ef598ce15d11da6e49f18e9b7a513dff390
rcvalenteai <44619673+rcvalenteai@users.noreply.github.com>
2021-04-09 12:10:55 -0400

    basic ui

---
+++ lib/test-main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:amplify_flutter/amplify.dart';
+import 'package:amplify_analytics_pinpoint/amplify_analytics_pinpoint.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'amplifyconfiguration.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _amplifyConfigured = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  void _configureAmplify() async {
+    if (!mounted) return;
+
+    // Add Pinpoint and Cognito Plugins
+    Amplify.addPlugin(AmplifyAnalyticsPinpoint());
+    Amplify.addPlugin(AmplifyAuthCognito());
+
+    // Once Plugins are added, configure Amplify
+    // Note: Amplify can only be configured once.
+    try {
+      await Amplify.configure(amplifyconfig);
+    } on AmplifyAlreadyConfiguredException {
+      print("Amplify was already configured. Was the app restarted?");
+    }
+    try {
+      setState(() {
+        _amplifyConfigured = true;
+      });
+    } catch (e) {
+      print(e);
+    }
+
+  }
+
+// Send an event to Pinpoint
+  void _recordEvent() async {
+    AnalyticsEvent event = AnalyticsEvent('test');
+    event.properties.addBoolProperty('boolKey', true);
+    event.properties.addDoubleProperty('doubleKey', 10.0);
+    event.properties.addIntProperty('intKey', 10);
+    event.properties.addStringProperty('stringKey', 'stringValue');
+    Amplify.Analytics.recordEvent(event: event);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Amplify Core example app'),
+            ),
+            body: ListView(padding: EdgeInsets.all(10.0), children: <Widget>[
+              Center(
+                child: Column (
+                    children: [
+                      const Padding(padding: EdgeInsets.all(5.0)),
+                      Text(
+                          _amplifyConfigured ? 'configured' : 'not yet configured'
+                      ),
+                      ElevatedButton(
+                          onPressed: _amplifyConfigured ? _recordEvent : null,
+                          child: const Text('record event')
+                      )
+                    ]
+                ),
+              )
+            ])
+        )
+    );
+  }
+}
