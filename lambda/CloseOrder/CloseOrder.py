5d82861e5d2890548b93f9bc73913d2861e27fae
Noel Qiao <noelqiao88@gmail.com>
2021-04-29 23:06:11 -0400

    Add close order lambda

---
+++ lambda/CloseOrder/CloseOrder.py
@@ -0,0 +1,62 @@
+import sys
+import logging
+import rds_config
+import pymysql
+import json
+import re
+
+#rds settings
+rds_host = "meal-plan-db.cssril6qx5ub.us-east-2.rds.amazonaws.com"
+name = rds_config.db_username
+password = rds_config.db_password
+db_name = rds_config.db_name
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def handler(event, context):
+    group_url = event['groupUrl']
+
+    match = re.match(r".*?group\?groupId=(\d+)", group_url)
+    if match is None:
+        return {
+            'errorMessage': 'Cannot extract groupId from groupUrl'
+        }
+    group_id = match.group(1)
+
+    try:
+        conn = pymysql.Connect(host=rds_host, port=3306, user=name, passwd=password, db=db_name, connect_timeout=5, cursorclass=pymysql.cursors.DictCursor)
+    except pymysql.MySQLError as e:
+        logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
+        logger.error(e)
+        sys.exit()
+    logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
+
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM GROCERY_PROJECT_DB.OrderGroups WHERE order_group_id = '{}'".format(group_id))
+        group = cur.fetchone()
+        if not group:
+            return {
+                'errorMessage': 'Group doesn\'t exist.'
+            }
+        logger.info("Group Id: {}".format(group['order_group_id']))
+
+        cur.execute("UPDATE GROCERY_PROJECT_DB.Order SET is_open = 0 WHERE order_group_id = '{}'".format(group_id))
+        conn.commit()
+
+    cur.close()
+    del cur
+    conn.close()
+
+    body = {
+        'groupLocation': 'group?groupId={}'.format(group_id)
+    }
+
+    response = {
+    'statusCode': 201,
+    'body': body 
+    }
+    logger.info(response)
+
+    return response
